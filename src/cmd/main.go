ddd2a311a86e08ef07b7cc102895358dbad50c8c
Dario Cantella <dcantella@2chc5l.onmicrosoft.com>
2024-10-28 17:43:23 +0100

    Refactor build and test commands to target specific source file and directory

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -6,20 +6,18 @@ import (
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
-
 	"github.com/Dariocent/time-checker-go/cmd/api"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	fmt.Println("The current time is: ", time.Now())
 
 	APIServer := api.NewAPIServer(os.Getenv("ADDR"), nil)
-	err = APIServer.Run()
+	err := APIServer.Run()
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
